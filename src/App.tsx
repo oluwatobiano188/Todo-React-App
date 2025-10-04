d7bfb2225a1d912ac65a01cf22e3694a0a7d769c
oluwatobi <isholaoluwatobi10@gmail.com>
2025-10-04 11:29:18 +0100

    push

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React...<br />
+          <h3>WELCOME TO MY PAGE... 😀  </h3>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
